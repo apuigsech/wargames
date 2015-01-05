b81930fd2d6a4f12ae83d24c0c99ff76dfbd270e
Albert Puigsech Galicia <albert@puigsech.com>
2015-01-05 16:11:46 +0100

    Add matasano crypto challenge (set 1 challenge 7)

---
+++ matasano/crypto-challenges/set 1/challenge 7/run.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# The matasano crypto challenges - Set 1 Challenge 7 (http://cryptopals.com/sets/1/challenges/7/)
+#
+# Copyright (c) 2015 - Albert Puigsech Galicia (albert@puigsech.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import base64
+import sys
+
+from Crypto.Cipher import AES
+
+
+
+def main(argv):
+	with open('7.txt') as f:
+		ct = base64.b64decode(f.read())
+
+	ecb_aes = AES.new("YELLOW SUBMARINE", AES.MODE_ECB)
+	pt = ecb_aes.decrypt(ct)
+
+	print pt
+
+
+
+if __name__ == "__main__":
+   main(sys.argv)
