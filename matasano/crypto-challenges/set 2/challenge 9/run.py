e4360ec10feb1495d979422fab6d0cd1694e3ec7
Albert Puigsech Galicia <albert@puigsech.com>
2015-01-05 20:30:43 +0100

    Add matasano crypto challenge (set 2 challenge 9)

---
+++ matasano/crypto-challenges/set 2/challenge 9/run.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# The matasano crypto challenges - Set 2 Challenge 9 (http://cryptopals.com/sets/2/challenges/9/)
+#
+# Copyright (c) 2015 - Albert Puigsech Galicia (albert@puigsech.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import sys
+
+def block_add_padding(block, blocklen=16):
+	padlen = blocklen-len(block)
+	return block + chr(padlen)*padlen
+
+
+def main(argv):
+	block="YELLOW SUBMARINE"
+
+	print block_add_padding(block, 20)
+
+
+if __name__ == "__main__":
+   main(sys.argv)
