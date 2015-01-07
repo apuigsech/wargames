866e2a50934c454aaf4494136f32f1656e019181
Albert Puigsech Galicia <albert@puigsech.com>
2015-01-07 02:09:52 +0100

    Add matasano crypto challenge (set 2 challenge 10)

---
+++ matasano/crypto-challenges/set 2/challenge 10/run.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# The matasano crypto challenges - Set 2 Challenge 10 (http://cryptopals.com/sets/2/challenges/10/)
+#
+# Copyright (c) 2015 - Albert Puigsech Galicia (albert@puigsech.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import base64
+import sys
+from cryptohelper import *
+
+
+def main(argv):
+	with open('10.txt') as f:
+		ct = ct = base64.b64decode(f.read())
+
+	print decrypt_block_CBC(ct, 16, "\x00"*16, "YELLOW SUBMARINE", decrypt_block_AES)
+
+
+if __name__ == "__main__":
+   main(sys.argv)
