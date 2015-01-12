0a949aeaa6854e77f2d1a9797afa06045bb8f1bb
Albert Puigsech Galicia <albert@puigsech.com>
2015-01-12 20:20:45 +0100

    Add matasano crypto challenge (set 3 challenge 18)

---
+++ matasano/crypto-challenges/set 3/challenge 18/run.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# The matasano crypto challenges - Set 3 Challenge 18 (http://cryptopals.com/sets/3/challenges/18/)
+#
+# Copyright (c) 2015 - Albert Puigsech Galicia (albert@puigsech.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import base64
+import sys
+
+from cryptohelper import *
+
+
+pt = base64.b64decode("L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==")
+key = "YELLOW SUBMARINE"
+
+
+def main(argv):
+	print  decrypt_block_CTR(pt, 16, "\x00"*8, key, encrypt_block_AES)
+
+
+if __name__ == "__main__":
+	main(sys.argv)
