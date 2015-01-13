c80afd12726882adf781e06054d18108ceb1525e
Albert Puigsech Galicia <albert@puigsech.com>
2015-01-13 01:16:41 +0100

    Add matasano crypto challenge (set 3 challenge 19)

---
+++ matasano/crypto-challenges/set 3/challenge 19/run.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+# The matasano crypto challenges - Set 3 Challenge 19 (http://cryptopals.com/sets/3/challenges/19/)
+#
+# Copyright (c) 2015 - Albert Puigsech Galicia (albert@puigsech.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import base64
+import random
+import itertools
+import operator
+import sys
+
+from cryptohelper import *
+
+
+key = ''.join([chr(random.randint(0,255)) for i in range(16)])
+
+
+data = [
+	"SSBoYXZlIG1ldCB0aGVtIGF0IGNsb3NlIG9mIGRheQ==",
+	"Q29taW5nIHdpdGggdml2aWQgZmFjZXM=",
+	"RnJvbSBjb3VudGVyIG9yIGRlc2sgYW1vbmcgZ3JleQ==",
+	"RWlnaHRlZW50aC1jZW50dXJ5IGhvdXNlcy4=",
+	"SSBoYXZlIHBhc3NlZCB3aXRoIGEgbm9kIG9mIHRoZSBoZWFk",
+	"T3IgcG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
+	"T3IgaGF2ZSBsaW5nZXJlZCBhd2hpbGUgYW5kIHNhaWQ=",
+	"UG9saXRlIG1lYW5pbmdsZXNzIHdvcmRzLA==",
+	"QW5kIHRob3VnaHQgYmVmb3JlIEkgaGFkIGRvbmU=",
+	"T2YgYSBtb2NraW5nIHRhbGUgb3IgYSBnaWJl",
+	"VG8gcGxlYXNlIGEgY29tcGFuaW9u",
+	"QXJvdW5kIHRoZSBmaXJlIGF0IHRoZSBjbHViLA==",
+	"QmVpbmcgY2VydGFpbiB0aGF0IHRoZXkgYW5kIEk=",
+	"QnV0IGxpdmVkIHdoZXJlIG1vdGxleSBpcyB3b3JuOg==",
+	"QWxsIGNoYW5nZWQsIGNoYW5nZWQgdXR0ZXJseTo=",
+	"QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4=",
+	"VGhhdCB3b21hbidzIGRheXMgd2VyZSBzcGVudA==",
+	"SW4gaWdub3JhbnQgZ29vZCB3aWxsLA==",
+	"SGVyIG5pZ2h0cyBpbiBhcmd1bWVudA==",
+	"VW50aWwgaGVyIHZvaWNlIGdyZXcgc2hyaWxsLg==",
+	"V2hhdCB2b2ljZSBtb3JlIHN3ZWV0IHRoYW4gaGVycw==",
+	"V2hlbiB5b3VuZyBhbmQgYmVhdXRpZnVsLA==",
+	"U2hlIHJvZGUgdG8gaGFycmllcnM/",
+	"VGhpcyBtYW4gaGFkIGtlcHQgYSBzY2hvb2w=",
+	"QW5kIHJvZGUgb3VyIHdpbmdlZCBob3JzZS4=",
+	"VGhpcyBvdGhlciBoaXMgaGVscGVyIGFuZCBmcmllbmQ=",
+	"V2FzIGNvbWluZyBpbnRvIGhpcyBmb3JjZTs=",
+	"SGUgbWlnaHQgaGF2ZSB3b24gZmFtZSBpbiB0aGUgZW5kLA==",
+	"U28gc2Vuc2l0aXZlIGhpcyBuYXR1cmUgc2VlbWVkLA==",
+	"U28gZGFyaW5nIGFuZCBzd2VldCBoaXMgdGhvdWdodC4=",
+	"VGhpcyBvdGhlciBtYW4gSSBoYWQgZHJlYW1lZA==",
+	"QSBkcnVua2VuLCB2YWluLWdsb3Jpb3VzIGxvdXQu",
+	"SGUgaGFkIGRvbmUgbW9zdCBiaXR0ZXIgd3Jvbmc=",
+	"VG8gc29tZSB3aG8gYXJlIG5lYXIgbXkgaGVhcnQs",
+	"WWV0IEkgbnVtYmVyIGhpbSBpbiB0aGUgc29uZzs=",
+	"SGUsIHRvbywgaGFzIHJlc2lnbmVkIGhpcyBwYXJ0",
+	"SW4gdGhlIGNhc3VhbCBjb21lZHk7",
+	"SGUsIHRvbywgaGFzIGJlZW4gY2hhbmdlZCBpbiBoaXMgdHVybiw=",
+	"VHJhbnNmb3JtZWQgdXR0ZXJseTo=",
+	"QSB0ZXJyaWJsZSBiZWF1dHkgaXMgYm9ybi4="
+]
+
+
+def main(argv):
+	for i in range(len(data)):
+		data[i] = encrypt_block_CTR(base64.b64decode(data[i]), 16, "\x00"*16, key, encrypt_block_AES, True)
+	ct_list = data
+
+
+	ks = keystream_from_many_time_pad(ct_list, dict(freq_eng, **{' ':15, ':':2, ';':2}))
+
+	for i in range(len(data)):
+		print strxor(data[i], ks)
+
+
+if __name__ == "__main__":
+	main(sys.argv)
